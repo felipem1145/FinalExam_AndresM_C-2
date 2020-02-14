851ec99b6bd6a17d772f84b6a1f149b66e34b7be
Andres <felipem1145@gmail.com>
2020-02-14 18:50:37 -0500

    Final Exam

---
+++ FinalExam_AndresM/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace FinalExam_AndresM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lstOutput = new System.Windows.Forms.ListBox();
+            this.btnFilter = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(147, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(147, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "First Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(147, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Last Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(147, 100);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Phone";
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(231, 43);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(134, 20);
+            this.txtFirstName.TabIndex = 4;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(231, 71);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(134, 20);
+            this.txtLastName.TabIndex = 5;
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(231, 17);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(134, 20);
+            this.txtID.TabIndex = 6;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(231, 97);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(134, 20);
+            this.txtPhone.TabIndex = 7;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(231, 123);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(134, 20);
+            this.txtEmail.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(147, 126);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Email";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(110, 166);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 10;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.BtnAdd_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(216, 166);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 11;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.BtnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(324, 166);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 12;
+            this.btnDelete.Text = "Remove";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.BtnDelete_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(176, 227);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(95, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Last Name SMITH";
+            // 
+            // lstOutput
+            // 
+            this.lstOutput.FormattingEnabled = true;
+            this.lstOutput.Location = new System.Drawing.Point(131, 251);
+            this.lstOutput.Name = "lstOutput";
+            this.lstOutput.Size = new System.Drawing.Size(278, 134);
+            this.lstOutput.TabIndex = 15;
+            // 
+            // btnFilter
+            // 
+            this.btnFilter.Location = new System.Drawing.Point(277, 222);
+            this.btnFilter.Name = "btnFilter";
+            this.btnFilter.Size = new System.Drawing.Size(75, 23);
+            this.btnFilter.TabIndex = 16;
+            this.btnFilter.Text = "Filter";
+            this.btnFilter.UseVisualStyleBackColor = true;
+            this.btnFilter.Click += new System.EventHandler(this.BtnFilter_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(199, 405);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(121, 23);
+            this.btnSave.TabIndex = 17;
+            this.btnSave.Text = "Save in a File";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.BtnSave_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(199, 452);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(121, 23);
+            this.btnLoad.TabIndex = 18;
+            this.btnLoad.Text = "Load from File";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.BtnLoad_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 540);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnFilter);
+            this.Controls.Add(this.lstOutput);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ListBox lstOutput;
+        private System.Windows.Forms.Button btnFilter;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnLoad;
+    }
+}
+
---
+++ FinalExam_AndresM/Form1.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace FinalExam_AndresM
+{
+    public partial class Form1 : Form
+    {
+        string connectStr = "server = INSTRUCTORIT; database = PhoneBook; user ID = ProfileUser; password = ProfileUser2019";
+
+        List<Contact> listContacts = new List<Contact>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        //INSERT 
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            
+
+            try
+            {
+                string ID = txtID.Text;
+                if(ID == "")
+                {
+                    string sqlInsertCommand;
+                    using (SqlConnection myConn = new SqlConnection(connectStr))
+                    {
+                        Contact addingContact = new Contact();
+
+                        addingContact.FirstName = txtFirstName.Text;
+                        addingContact.LastName = txtLastName.Text;
+                        addingContact.Phone = txtPhone.Text;
+                        addingContact.Email = txtEmail.Text;
+
+                        listContacts.Add(addingContact);
+
+                        myConn.Open();
+                        sqlInsertCommand = "INSERT INTO Contact" + "(FirstName, LastName,Phone, Email) VALUES ('" + addingContact.FirstName + "','" + addingContact.LastName + "','" + addingContact.Phone + "','" + addingContact.Email + "');";
+                        SqlCommand myInsertCommand = new SqlCommand(sqlInsertCommand, myConn);
+                        myInsertCommand.ExecuteNonQuery();
+                        myConn.Close();
+
+                    }
+                    MessageBox.Show("Contact added successfully!");
+                }else
+                    MessageBox.Show("Please don't add ID number");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        //UPDATE
+
+        private void BtnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string sqlUpdateCommand;
+                using (SqlConnection myConn = new SqlConnection(connectStr))
+                {
+                    Contact updateContact = new Contact();
+                    updateContact.ID = int.Parse(txtID.Text);
+                    updateContact.FirstName = txtFirstName.Text;
+                    updateContact.LastName = txtLastName.Text;
+                    updateContact.Phone = txtPhone.Text;
+                    updateContact.Email = txtEmail.Text;
+
+                    myConn.Open();
+                    sqlUpdateCommand = "UPDATE Contact " + "SET FirstName = '" + updateContact.FirstName + "', LastName = '" + updateContact.LastName + "', Phone = ' "+updateContact.Phone+ "', Email = '" + updateContact.Email + "' WHERE ID =" + updateContact.ID + ";";
+                    SqlCommand myUpdateCommand = new SqlCommand(sqlUpdateCommand, myConn);
+                    myUpdateCommand.ExecuteNonQuery();
+                    myConn.Close();
+                }
+
+                MessageBox.Show("Contact updated successfully!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        //DELETE
+        private void BtnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string sqlDeleteCommand;
+                using (SqlConnection myConn = new SqlConnection(connectStr))
+                {
+                    Contact deleteContact = new Contact();
+                    deleteContact.ID = int.Parse(txtID.Text);
+
+
+                    myConn.Open();
+                    sqlDeleteCommand = "DELETE FROM Contact WHERE ID =" + deleteContact.ID + ";";
+                    SqlCommand myDeleteCommand = new SqlCommand(sqlDeleteCommand, myConn);
+                    myDeleteCommand.ExecuteNonQuery();
+                    myConn.Close();
+
+
+                }
+                MessageBox.Show("Contact removed successfully!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+        //SELECTS FROM TABLE AND USE LINQ
+
+            //****************THERE IS NOT ANYONE WITH LAST NAME SMITH, I TESTED WITH DIFFERENT LASTNAMES **************
+
+        private void BtnFilter_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+
+                lstOutput.Items.Clear();
+                   using (SqlConnection myConnection = new SqlConnection(connectStr))
+                    {
+                        string sqlCommand;
+                        myConnection.Open();
+                        sqlCommand = "SELECT * FROM Contact";
+                        SqlCommand myCommand = new SqlCommand(sqlCommand, myConnection);
+                        SqlDataReader myReader = myCommand.ExecuteReader(); ;
+
+                        while (myReader.Read())
+                        {
+                            Contact tempContact = new Contact();
+                            tempContact.ID = int.Parse(myReader["ID"].ToString());
+                            tempContact.FirstName = myReader["FirstName"].ToString();
+                            tempContact.LastName = myReader["LastName"].ToString();
+                            tempContact.Phone = myReader["Phone"].ToString();
+                            tempContact.Email = myReader["Email"].ToString();
+
+                        listContacts.Add(tempContact);
+
+                    }
+                        myReader.Close();
+                        myConnection.Close();
+                    MessageBox.Show("Successful");
+                    
+                    int k;
+                    Contact tempCont = new Contact();
+
+                    List<Contact> contactFilter = new List<Contact>();
+
+
+                    var query = from contactF in listContacts
+                                where contactF.LastName == "Smith"
+                                select contactF;
+                    contactFilter = query.ToList<Contact>();
+
+
+                    for (k = 0; k < contactFilter.Count; k++)
+                    {
+                        tempCont = contactFilter[k];
+                        lstOutput.Items.Add("ID = " + tempCont.ID.ToString() + " FirstName = " + tempCont.FirstName + " LastName = " + tempCont.LastName +" Phone = "+tempCont.Phone+ " Email = " + tempCont.Email);
+                    }
+                    
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        //SELECTS DATA AND SAVE IT INTO A TEXT FILE IN C:\
+
+        private void BtnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection myConnection = new SqlConnection(connectStr))
+                {
+                    string sqlCommand;
+                    myConnection.Open();
+                    sqlCommand = "SELECT * FROM Contact";
+                    SqlCommand myCommand = new SqlCommand(sqlCommand, myConnection);
+                    SqlDataReader myReader = myCommand.ExecuteReader(); ;
+
+                    while (myReader.Read())
+                    {
+                        Contact tempContact = new Contact();
+                        tempContact.ID = int.Parse(myReader["ID"].ToString());
+                        tempContact.FirstName = myReader["FirstName"].ToString();
+                        tempContact.LastName = myReader["LastName"].ToString();
+                        tempContact.Phone = myReader["Phone"].ToString();
+                        tempContact.Email = myReader["Email"].ToString();
+
+                        listContacts.Add(tempContact);
+
+                    }
+                    myReader.Close();
+                    myConnection.Close();
+                }
+
+                string pathName = @"C:\";
+
+                string fileName = @"DatabaseBackup_2020-02-14.txt";
+                int k;
+                StreamWriter sw = new StreamWriter(Path.Combine(pathName, fileName), false);
+
+                for (k = 0; k < listContacts.Count; k++)
+                {
+                    Contact tempContact = new Contact();
+
+                    tempContact = listContacts[k];
+                    sw.WriteLine("ID = " + tempContact.ID.ToString() + " FirstName = " + tempContact.FirstName + " LastName = " + tempContact.LastName + " Phone = " + tempContact.Phone + " Email = " + tempContact.Email);
+
+                }
+                sw.Close();
+                MessageBox.Show("Contacts saved!");
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+        //READS INFO FROM THE TEXT FILE 
+
+        private void BtnLoad_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string pathName = @"C:\Andres";
+
+                string fileName = @"DatabaseBackup_2020-02-14.txt";
+
+                string pfn = Path.Combine(pathName, fileName);
+
+                if (File.Exists(pfn) == true)
+                {
+                    StreamReader sr = new StreamReader(pfn);
+                    while (true)
+                    {
+                        string line = sr.ReadLine();
+                        if (line == null) break;
+                        lstOutput.Items.Add(line);
+
+                    }
+                    sr.Close();
+                }
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        //CLASS CONTACT CREATED IN A DIFERENT FILE Contact.cs 
+    }
+}
